d50e0984cb27a3077384856cb242536f59108ce4
rnoth <rnoth@tutanota.com>
2016-08-23 17:57:35 -0400

    added a script implementing a mark and recall feature

---
+++ mark.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+TEMPDIR=~/tmp
+
+[ -z $1 ] && echo "No enough arguments" && exit 1
+
+mark_window() {
+	echo `pfw` > $TEMPDIR/.marks/"$mark".wid
+}
+
+list_marks() {
+	ls $TEMPDIR/.marks
+}
+
+get_wid(){
+	cat $TEMPDIR/.marks/"$target"
+}
+
+if_no_mark(){
+	[ $? != 0 ] && echo "mark.sh: $target not found"
+}
+
+case $1 in
+	mark)
+		mark=`list_marks | dmenu -b -p "Mark: "`
+		mark_window
+		;;
+	qmark)
+		mark=$2
+		mark_window
+		;;
+	recall)
+		target=`list_marks | dmenu -b -p "Recall: "`
+		wtf `get_wid`
+		mapw -m `pfw`
+		if_no_mark
+		;;
+	qrecall)
+		target=$2
+		wtf `get_wid`
+		if_no_mark
+		;;
+	unmark)
+		target=`list_marks | dmenu -b -p "Recall: "`
+		rm $TEMPDIR/.marks/"$target".wid
+		if_no_mark
+		;;
+	qunmark)
+		target=$2
+		rm $TEMPDIR/.marks/"$target".wid
+		if_no_mark
+		;;
+	hide)
+		target=`list_marks | dmenu -b -p "Hide: "`
+		mapw -u `get_wid`
+		;;
+	qhide)
+		target=$2
+		mapw -u `cat $TEMPDIR/.marks/"$target".wid`
+		;;
+esac	
